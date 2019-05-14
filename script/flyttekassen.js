63f70d9cf8a694dbf92faaa9681fa075209e2b57
ChristineDue <christine_due@yhaoo.dk>
2019-05-14 14:12:40 +0200

    .js. oprettet

---
+++ script/flyttekassen.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+document.getElementById("klikSove").addEventListener('click', function () {
+	document.getElementById('sove').innerHTML = "<div><p>hej med dig!</p></div>"
+})
+
